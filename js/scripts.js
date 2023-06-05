5262ed60fd3c76600bce4b760d880725631ff09b
xoxo2812 <nachaelim560@gmail.com>
2023-06-05 17:29:18 +0900

    연습하자

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('header .swiper', {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+  
